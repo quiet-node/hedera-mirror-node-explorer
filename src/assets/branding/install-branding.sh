148c64f00faa5e818965e5dfdd4f514f22e69e8d
Simon Viénot <simon.vienot@icloud.com>
2025-03-26 16:30:55 +0100

    chore: remove unused assets (#1780)
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- src/assets/branding/install-branding.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Install possible assets customization
-BRANDING_DIR="${BRANDING_LOCATION:-./branding}"
-cp ${BRANDING_DIR}/public/* ./public/ 2>/dev/null | :
-cp -R ${BRANDING_DIR}/public/.well-known ./public/ 2>/dev/null | :
-cp ${BRANDING_DIR}/.env ./ 2>/dev/null | :
-cp ${BRANDING_DIR}/index.html ./ 2>/dev/null | :
